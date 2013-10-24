46fb1253443255e00cbd44ed62294593f669392d
azee <azee@yandex-team.ru>
2013-10-24 17:36:38 +0400

    User repository

---
+++ template-dal/src/main/java/com/mycompany/template/repositories/UserRepository.java
@@ -0,0 +1,18 @@
+package com.mycompany.template.repositories;
+
+import com.mycompany.template.beans.User;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: azee
+  */
+public interface UserRepository extends PagingAndSortingRepository<User, String>{
+
+    @Query("{ 'token' : ?0 }")
+    User findByToken(String token);
+
+    @Query ("{ 'name' : ?0 }")
+    User findByName(String name);
+}
