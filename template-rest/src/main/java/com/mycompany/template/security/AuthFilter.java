bad67592576cdc06e344e4c940e69a92f8cc7590
azee <azee@yandex-team.ru>
2013-10-28 17:40:08 +0400

    Spring security

---
+++ template-rest/src/main/java/com/mycompany/template/security/AuthFilter.java
@@ -0,0 +1,61 @@
+package com.mycompany.template.security;
+
+import com.mycompany.template.beans.User;
+import com.mycompany.template.services.UserService;
+import com.mycompany.template.utils.UserDataUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: azee
+ */
+public class AuthFilter extends AbstractPreAuthenticatedProcessingFilter {
+
+    //private static String SID_COOKIE_NAME = "Session_id";
+    private static String GUEST_VAL = "Guest";
+    private final static Logger log = Logger.getLogger(AuthFilter.class);
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    UserDataUtils userDataUtils;
+
+    @Override
+    protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+        User user;
+
+        Cookie sidCookie = userDataUtils.getSidFromRequest(request);
+        if (sidCookie != null){
+            log.info("Found sid cookie, trying to authenticate");
+        }
+        else {
+            log.info("Couldn't find sid cookie");
+            return GUEST_VAL;
+        }
+
+        try {
+            user = userService.checkSid(request);
+        } catch (Exception e) {
+            log.error("Couldn't get a user by sid. Will be authorized as a Guest");
+            return GUEST_VAL;
+        }
+
+        if (user != null) {
+            log.info("Filter: Getting user: " + user.getName());
+            return user.getName().trim();
+        }
+
+        return GUEST_VAL;
+    }
+
+    @Override
+    protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
+        return "";
+    }
+}
---
+++ template-rest/src/main/java/com/mycompany/template/security/PreAuthUserDetails.java
@@ -0,0 +1,47 @@
+package com.mycompany.template.security;
+
+import com.mycompany.template.beans.Role;
+import com.mycompany.template.beans.User;
+import com.mycompany.template.services.UserService;
+import com.mycompany.template.utils.UserDataUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: azee
+ */
+public class PreAuthUserDetails implements AuthenticationUserDetailsService {
+
+    private final static Logger log = Logger.getLogger(PreAuthUserDetails.class);
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    UserDataUtils userDataUtils;
+
+    public UserDetails loadUserDetails(Authentication authUser) throws UsernameNotFoundException {
+        try {
+            User user = userService.getUserByName(authUser.getName());
+            if(user == null){
+                log.info("User " + authUser.getName() + " not found, granting Guest Role");
+                user = new User();
+                user.setName(authUser.getName());
+                user.getRoles().add(Role.ROLE_GUEST);
+            }
+
+            log.info("Setting up a user " + user.getName());
+            UserDetailsImpl userDetails = new UserDetailsImpl();
+            userDetails.setUser(user);
+            userDetails.setRoles();
+            return userDetails;
+        } catch (Exception e) {
+            throw new UsernameNotFoundException(e.getMessage());
+        }
+    }
+}
